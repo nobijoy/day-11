2431db5aa8c1bdc6ee79bca481fc4080e27f78bc
nobijoy <nobijoy1446@gmail.com>
2022-01-31 12:19:59 +0600

    dfffs

--- app/classes/Example.php
+++ app/classes/Example.php
@@ -5,9 +5,81 @@ class Example
     public $student = [];
 
 
-    public function index(){
+    public function index()
+    {
+        $this->student = [
+            0=>[
+                'name'      => 'jahid',
+                'email'     => 'jahid@gmail.com',
+                'mobile'    => [
+                    'mobile1' => '0192562568',
+                    'mobile2' => '0192562568',
+                ]
+            ],
+            1=>[
+                'name'      => 'Fahim',
+                'email'     => 'Fahim@gmail.com',
+                'mobile'    => [
+                    'mobile1' => '0192562568',
+                    'mobile2' => '0192562568',
+                ]
+            ],
+            2 => 'BITM',
+            3=>[
+                'name'      => 'CR PAGLA',
+                'email'     => 'Fahim@gmail.com',
+                'mobile'    => [
+                    'mobile1' => '0192562568',
+                    'mobile2' => '0192562568',
+                ]
+            ]
+        ];
 
-        $this->student = [10, 20, 'BITM', 10.20, true, false];
-        echo $this->student;
+        echo '<pre>';
+//        print_r($this->student);
+        var_dump($this->student);
+        $_
 
+//        foreach ($this->student as$key => $item){
+//            if($key == 0){
+//                echo $item['name'].' '.$item['email'].' '.$item['mobile'].'<br/>';
+//                break;
+//            }
+//        }
+
+//        foreach ($this->student as $item){
+//            if (is_array($item)){
+//                foreach ($item as $value){
+//                    if(is_array($value)){
+//                        foreach ($value as $v_value){
+//                            echo $v_value;
+//                        }
+//                    }
+//                    else
+//                    {
+//                        echo $value.' ';
+//                    }
+//
+//                }
+//            }
+//           else{
+//               echo $item;
+//           }
+//            echo '<br/>';
+//        }
+
+
+//        echo $this->student[0]['name'].'<br/>';
+//        echo $this->student[0]['email'].'<br/>';
+//        echo $this->student[0]['mobile'].'<br/>';
+
+
+
+//        $this->student = [10, 20, 'BITM', 10.20, true, false];
+//        foreach ($this->student as $key => $student){
+//            echo 'index no- '. $key.' value -'.$student. '<br/ > ';
+//        }
+
+
+    }
 }
--- index.php
+++ index.php
@@ -1,6 +1,71 @@
-<?php
-require_once 'vendor/autoload.php';
-use App\classes\Example;
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Home</title>
+    <link rel="stylesheet" href="assets/css/bootstrap.css">
+    <link rel="stylesheet" href="assets/css/all.css">
+    <link rel="stylesheet" href="assets/css/styles.css">
+</head>
+<body>
 
-$example = new Example();
-$example->index();
+    <nav class="navbar navbar-expand-md navbar-dark bg-dark">
+        <div class="container">
+            <a href="" class="navbar-brand">LOGO</a>
+            <ul class="navbar-nav">
+                <li><a href="" class="nav-link">Example1</a></li>
+                <li><a href="" class="nav-link">Example1</a></li>
+            </ul>
+        </div>
+    </nav>
+
+    <section class="py-5 bg-light">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-8">
+                    <div class="card">
+                        <div class="card-header">Please Fill up</div>
+                        <div class="card-body">
+                            <form>
+                                <div class="form-group row">
+                                    <label class="col-md-3 col-form-label">First Name: </label>
+                                    <div class="col-md-9">
+                                        <input type="text" class="form-control"/>
+                                    </div>
+                                </div>
+                                <div class="form-group row">
+                                    <label class="col-md-3 col-form-label">Last Name: </label>
+                                    <div class="col-md-9">
+                                        <input type="text" class="form-control"/>
+                                    </div>
+                                </div>
+                                <div class="form-group row">
+                                    <label class="col-md-3 col-form-label">Full Name: </label>
+                                    <div class="col-md-9">
+                                        <input type="text" class="form-control"/>
+                                    </div>
+                                </div>
+                                <div class="form-group row">
+                                    <label class="col-md-3 col-form-label"></label>
+                                    <div class="col-md-9">
+                                        <input type="button" class="btn btn-outline-success" value="submit"/>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+
+
+<script src="assets/js/bootstrap.js"></script>
+<script src="assets/js/jquery-3.6.0.js"></script>
+
+</body>
+</html>
